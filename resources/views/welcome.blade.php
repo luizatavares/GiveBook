4e1dc96a204c551330e6b94523a473958ec5ed3f
Luiza <luiza.tavarespenido@gmail.com>
2017-10-21 18:43:57 -0200

    Adicionada nova categoria

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
             <a href="#" class="list-group-item">Category 1</a>
             <a href="#" class="list-group-item">Category 2</a>
             <a href="#" class="list-group-item">Category 3</a>
+            <a href="#" class="list-group-item">Category 4</a>
           </div>
         </div>
         <!-- /.col-lg-3 -->
