b592cd803341a6a90c5031986e92ff6f999d8876
Luiza <luiza.tavarespenido@gmail.com>
2017-10-29 20:49:57 -0200

    ajuste rota

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+
 Route::group(['prefix' => 'painel', 'middleware'=>'auth'], function(){
 
 	Route::get('/meusdados', 'UsuariosController@meusdados')->name('meusdados');
