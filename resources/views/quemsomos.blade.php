4d9f175529162fbbf48af208faf834c39865f0fa
abreuermelinda <ermelinda.ti05n@gmail.com>
2017-11-02 16:19:06 -0200

    Criação pagina Quem somos

---
+++ resources/views/quemsomos.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+     <div class="row">
+
+        <div class="col-lg-12">
+          <h1 class="my-4 text-center">Sobre nós</h1>
+
+        </div>
+        <!-- /.col-lg-3 -->
+
+   <p class="text-center">
+     A proposta do projeto GiveBook é proporcionar um "Mural Digital" facilitador
+para doações de livros, incentivando a criação do hábito de leitura para toda a
+sociedade, visando principalmente acessibilidade para quem não possui condições
+financeiras de adquirir o produto, e ajudando de uma forma pratica à quem gostaria
+de doar conhecimento ao próximo. 
+   </p>
+    </div>
+</div>
+@endsection
